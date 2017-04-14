45b0da31607a83989ad245db420e5634139e95ff
Kathryn Klarich <kathryklarich@gmail.com>
2017-04-13 21:44:45 -0500

    added to-do list

--- rev_pol_not.py
+++ rev_pol_not.py
@@ -1,6 +1,13 @@
 # import operator module
 import operator
 
+# To-do list:
+# -add doc strings and better documentation
+# -add dictionary lookup for operators instead of if statements
+# -make it so that the program can accept other types of numbers
+# -add a main function which prints the final answer
+# -try additional test cases
+
 # holds the numbrs in the stack
 stack = []
 
@@ -14,22 +21,29 @@ def evaluate_input(*args):
     for arg in args:
         if arg in numbers:
             stack.append(arg)
+
         elif arg in operators:
             # call function that deals with numerical operations
-            eval_ops(arg, a=stack[-2], b=stack[-1])
+            eval_ops(symbol=arg, a=stack.pop(-2), b=stack.pop(-1))
+        # if none of the above conditions apply, tell the user to change input
         else:
             print('''
             Cannot read {}. Please use only numbers zero through
             10 and the following operators for addition (+), subtraction (-),
             division (/) and multipliation (*)'''.format(arg))
             break
+    return stack
 
 def eval_ops(symbol, a, b):
-    #create a dict to map input operators to operator functions
-    operator_dict = {'+': operator.add, '-': operator.sub, '*': operator.mul}
-    print('operator = {}, a = {}, b = {}'.format(symbol, a, b))
+    # create a dict to map input operators to operator functions
+    operator_dict = {'+': operator.add, '-': operator.sub, '*': operator.mul, '/': operator.truediv}
+    if symbol == '+':
+        stack.append(operator.add(a, b))
+    if symbol == '*':
+        stack.append(operator.mul(a, b))
+    if symbol == '-':
+        stack.append(operator.sub(a, b))
+    if symbol == '/':
+        stack.append(operator.truediv(a, b))
 
-
-
-
-evaluate_input(5, 1, 2, 4, '*', '+', 3, '-')
+evaluate_input(5, 1, 2, '+', 4, '*', '+', 3, '-')
